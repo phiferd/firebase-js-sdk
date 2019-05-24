a1508be36d1677e080b6ba50dcb912503a001edd
Mertcan Mermerkaya <mertcan@google.com>
2019-05-24 13:41:32 +0100

    Remove some object utility functions

--- packages/storage/src/implementation/requests.ts
+++ packages/storage/src/implementation/requests.ts
@@ -28,7 +28,6 @@ import * as errorsExports from './error';
 import { FirebaseStorageError } from './error';
 import { Location } from './location';
 import * as MetadataUtils from './metadata';
-import { clone } from '@firebase/util';
 import { RequestInfo } from './requestinfo';
 import * as type from './type';
 import * as UrlUtils from './url';
@@ -218,7 +217,7 @@ export function metadataForUpload_(
   blob: FbsBlob,
   opt_metadata?: Metadata | null
 ): Metadata {
-  let metadata = clone(opt_metadata);
+  let metadata = { ...opt_metadata };
   metadata['fullPath'] = location.path;
   metadata['size'] = blob.size();
   if (!metadata['contentType']) {
